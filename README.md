3ca3b45826b2df1b5543a331320b59b972663369
fathallah <fathallah.mohamed.a@gmail.com>
2025-06-22 13:17:55 +0200

    suppression du contenu

--- README.md
+++
@@ -1,1 +0,0 @@
-# mspr-manager-projet
--- back/src/main/java/com/mspr/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mspr.repositories;
-
-import com.mspr.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
